6d09a7224aa92588d0c7d85ffca125bffb562591
Mouhsine ELAOULA <elaoulamouhsine@gmail.com>
2026-01-10 23:20:39 +0100

    implémentations des models de l'app consultations

---
+++ consultations/admin.py
@@ -0,0 +1,56 @@
+from django.contrib import admin
+from .models import Acte, Consultation, Ordonnance
+
+@admin.register(Acte)
+class ActeAdmin(admin.ModelAdmin):
+    """
+    Gestion du catalogue des actes médicaux.
+    """
+    list_display = ('nom', 'tarif')
+    search_fields = ('nom',)
+    ordering = ('nom',)
+
+class OrdonnanceInline(admin.StackedInline):
+    """
+    Permet de voir/modifier l'ordonnance directement dans la page de la consultation.
+    """
+    model = Ordonnance
+    extra = 0 # Pas de ligne vide par défaut
+    can_delete = True
+
+@admin.register(Consultation)
+class ConsultationAdmin(admin.ModelAdmin):
+    list_display = ('date_consultation', 'get_patient', 'get_medecin', 'est_revision', 'tarif_total')
+    list_filter = ('date_consultation', 'est_revision')
+    search_fields = ('rdv__patient__user__last_name', 'rdv__patient__user__first_name', 'diagnostic')
+    autocomplete_fields = ['rdv'] # Utile si vous avez beaucoup de RDV
+    
+    # On ajoute l'ordonnance en bas de la fiche consultation
+    inlines = [OrdonnanceInline]
+    
+    # Configuration du champ ManyToMany (Actes) pour une sélection plus facile
+    filter_horizontal = ('actes',) 
+
+    def get_patient(self, obj):
+        return f"{obj.rdv.patient.user.last_name} {obj.rdv.patient.user.first_name}"
+    get_patient.short_description = 'Patient'
+    get_patient.admin_order_field = 'rdv__patient__user__last_name'
+
+    def get_medecin(self, obj):
+        return f"Dr. {obj.rdv.docteur.user.last_name}"
+    get_medecin.short_description = 'Médecin'
+
+    def tarif_total(self, obj):
+        """Calcule la somme des actes réalisés pour cette consultation"""
+        total = sum(acte.tarif for acte in obj.actes.all())
+        return f"{total} DH"
+    tarif_total.short_description = 'Total Actes'
+
+@admin.register(Ordonnance)
+class OrdonnanceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'get_consultation_str', 'date_ordonnance')
+    search_fields = ('traitement',)
+    
+    def get_consultation_str(self, obj):
+        return str(obj.consultation)
+    get_consultation_str.short_description = 'Consultation Associée'
---
+++ consultations/models.py
@@ -0,0 +1,79 @@
+from django.db import models
+
+class Acte(models.Model):
+    """
+    Catalogue des actes médicaux (ex: Consultation généraliste, ECG, Echographie...)
+    Table 'ACTE' du diagramme.
+    """
+    nom = models.CharField(max_length=150, verbose_name="Nom de l'acte")
+    tarif = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Tarif")
+
+    def __str__(self):
+        return f"{self.nom} ({self.tarif} DH)"
+
+class Consultation(models.Model):
+    """
+    Table 'CONSULTATION' du diagramme.
+    Reliée au RDV (planning) et peut contenir plusieurs Actes.
+    """
+    # Relation "Aboutir" 1..1 avec le RDV (Un RDV donne une Consultation)
+    # On utilise une chaîne de caractères 'planning.RDV' pour éviter les imports circulaires
+    rdv = models.OneToOneField(
+        'planning.RDV', 
+        on_delete=models.CASCADE, 
+        related_name='consultation',
+        verbose_name="Rendez-vous associé"
+    )
+    
+    # Relation "Composer" 1..n avec Acte (ManyToMany)
+    actes = models.ManyToManyField(
+        Acte, 
+        related_name='consultations',
+        verbose_name="Actes réalisés"
+    )
+
+    # Relation récursive "Est révision" / "Id_Cons_origine"
+    consultation_origine = models.ForeignKey(
+        'self', 
+        on_delete=models.SET_NULL, 
+        null=True, 
+        blank=True, 
+        related_name='revisions',
+        verbose_name="Consultation d'origine (si révision)"
+    )
+
+    date_consultation = models.DateField(auto_now_add=True, verbose_name="Date de consultation")
+    diagnostic = models.TextField(verbose_name="Diagnostic")
+    commentaires = models.TextField(blank=True, null=True, verbose_name="Commentaires")
+    
+    # Champ booléen explicite comme dans le diagramme (bien que déductible via consultation_origine)
+    est_revision = models.BooleanField(default=False, verbose_name="Est une révision")
+
+    def save(self, *args, **kwargs):
+        # Automatisation : si une consultation origine est définie, c'est une révision
+        if self.consultation_origine:
+            self.est_revision = True
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Consultation du {self.date_consultation} - {self.rdv.patient}"
+
+class Ordonnance(models.Model):
+    """
+    Table 'ORDONNANCE' du diagramme.
+    Relation "Délivrer" 1..1 avec Consultation.
+    """
+    consultation = models.OneToOneField(
+        Consultation, 
+        on_delete=models.CASCADE, 
+        related_name='ordonnance'
+    )
+    
+    date_ordonnance = models.DateField(auto_now_add=True, verbose_name="Date de l'ordonnance")
+    traitement = models.TextField(
+        verbose_name="Traitement (Médicaments et posologie)", 
+        help_text="Détaillez les médicaments prescrits ici."
+    )
+
+    def __str__(self):
+        return f"Ordonnance pour {self.consultation.rdv.patient}"
